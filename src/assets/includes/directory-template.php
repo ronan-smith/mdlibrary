4384a07ff4b08350e84ee7bf3f22adece2a977ab
Ronan Smith <46461128+ronan-smith@users.noreply.github.com>
2023-03-13 10:03:28 +0000

    Fix directory item padding and add folder colour

--- src/assets/includes/directory-template.php
+++ src/assets/includes/directory-template.php
@@ -5,15 +5,17 @@
   </div>
   <ul id="results">
     <?php foreach ($folders as $folder): ?>
-    <li>
-      <a href="<?=$folder?>"><?=formatDisplayText($folder)?></a>
-      <i class="fa-solid fa-folder-open"></i>
+    <li class="folder">
+      <a href="<?=$folder?>"><?=formatDisplayText($folder)?>
+        <i class="fa-solid fa-folder-open"></i>
+      </a>
     </li>
     <?php endforeach; ?>
     <?php foreach ($documents as $document): ?>
     <li>
-      <a href="<?=$document?>"><?=formatDisplayText($document)?></a>
-      <i class="fa-solid fa-file"></i>
+      <a href="<?=$document?>"><?=formatDisplayText($document)?>
+        <i class="fa-solid fa-file"></i>
+      </a>
     </li>
     <?php endforeach; ?>
   </ul>
